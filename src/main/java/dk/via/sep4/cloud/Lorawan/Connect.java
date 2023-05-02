bb85d55adc698d65529d45d472857e8fb203ad22
BekoLolek <bekololek@gmail.com>
2023-05-02 17:12:04 +0200

    Added json parsing and more

--- src/main/java/dk/via/sep4/cloud/Lorawan/Connect.java
+++ src/main/java/dk/via/sep4/cloud/Lorawan/Connect.java
@@ -1,7 +1,10 @@
 package dk.via.sep4.cloud.Lorawan;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dk.via.sep4.cloud.Persistance.SensorReading;
 import org.eclipse.paho.client.mqttv3.*;
+import org.thethingsnetwork.data.mqtt.Client;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.thethingsnetwork.data.common.messages.*;
 
@@ -9,45 +12,37 @@ public class Connect {
 
     public static void main(String[] args) throws MqttException {
 
-        String url = "wss://iotnet.cibicom.dk/app?";
+        String url = "wss://iotnet.cibicom.dk/app";
+        String clientId = MqttClient.generateClientId();
         String token = "vnoUeAAAABFpb3RuZXQudGVyYWNvbS5kawhxYha6idspsvrlQ4C7KWA=";
         String appkey = "E0597BF885F1F18CF896B91F8E211814";
         String joinEUI = "49B360EEE16A8D4C";
         String EUI_dev = "0004A30B0021B92F";
 
-        MqttClient client = new MqttClient(url, MqttClient.generateClientId());
-
-
-        client.setCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-                // Handle connection lost event
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                // Extract the payload from the MQTT message
-                byte[] payload = message.getPayload();
-                String data = new String(payload);
-                System.out.println("Received data: " + data);
-                double temperature = 1;
-                SensorReading reading = new SensorReading(temperature);
-            }
-
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-                // Handle delivery complete event
-            }
-        });
-
         MqttConnectOptions options = new MqttConnectOptions();
-        options.setUserName(joinEUI);
-        options.setPassword(appkey.toCharArray());
+        options.setUserName(token);
+
+        MqttClient client = new MqttClient(url, clientId);
         client.connect(options);
 
-        String uplinkTopic = String.format("%s/devices/+/up", joinEUI);
-        client.subscribe(uplinkTopic);
+        String topic = "application/" + joinEUI + "/device/" + EUI_dev + "/rx";
+        client.subscribe(topic);
+
+        client.setCallback(new MqttCallback() {
+            public void connectionLost(Throwable cause) {}
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                byte[] payload = message.getPayload();
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode rootNode = objectMapper.readTree(payload);
+                double temperature = rootNode.get("temperature").asDouble();
+                System.out.println("Temperature: " + temperature);
+                SensorReading reading = new SensorReading(temperature);
+            }
+            public void deliveryComplete(IMqttDeliveryToken token) {}
+        });
+
+
+
 
         while(true){
 
