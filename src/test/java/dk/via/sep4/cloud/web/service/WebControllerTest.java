8eb81416850328544d4c4e77ab872723aaa7e82e
Ieris19 <ieris19@outlook.com>
2023-05-02 21:46:35 +0200

    Updated Web-API test
    Abstracted the tests so that they can test any
    endpoint easily without code repetition. At least
    simple tests

--- src/test/java/dk/via/sep4/cloud/web/service/WebControllerTest.java
+++ src/test/java/dk/via/sep4/cloud/web/service/WebControllerTest.java
@@ -1,7 +1,6 @@
 package dk.via.sep4.cloud.web.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 import java.net.URI;
@@ -10,47 +9,61 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WebControllerTest {
-    private String portNumber = "8080";
+	private String portNumber = "8080";
 
-    @Test
-    void getLastReading() {
-        HttpRequest request;
-        try {
-            request = lastReadingRequest();
-        } catch (URISyntaxException e) {
-            System.out.println("URISyntaxException: " + e.getMessage());
-            e.printStackTrace();
-            assert false;
-            return;
-        }
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-            e.printStackTrace();
-            assert false;
-            return;
-        } catch (InterruptedException e) {
-            System.out.println("InterruptedException: " + e.getMessage());
-            e.printStackTrace();
-            assert false;
-            return;
-        }
-        System.out.println(response.body());
-        assertEquals(200, response.statusCode());
-    }
+	private void assertRequestStatus(HttpRequest request, int expectedStatusCode) {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpResponse<String> response;
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			System.out.println("IOException: " + e.getMessage());
+			e.printStackTrace();
+			assert false;
+			return;
+		} catch (InterruptedException e) {
+			System.out.println("InterruptedException: " + e.getMessage());
+			e.printStackTrace();
+			assert false;
+			return;
+		}
+		System.out.println(response.body());
+		assertEquals(expectedStatusCode, response.statusCode());
+	}
 
-    private HttpRequest lastReadingRequest() throws URISyntaxException {
-        String TARGET_URL = "http://localhost:" + portNumber + "/lastReading";
-        URI targetURI = new URI(TARGET_URL);
-        return HttpRequest.newBuilder()
-                .uri(targetURI)
-                .GET()
-                .build();
-    }
+	private HttpRequest getRequestForPath(String apiPath, String apiMethod) {
+		String TARGET_URL = "http://localhost:" + portNumber + apiPath;
+		URI targetURI;
+		try {
+			targetURI = new URI(TARGET_URL);
+		} catch (URISyntaxException e) {
+			System.out.println("URISyntaxException: " + e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+		HttpRequest.Builder requestBuilder = HttpRequest.newBuilder().uri(targetURI);
+		if (apiMethod.equalsIgnoreCase("GET")) {
+			requestBuilder.GET();
+		} else if (apiMethod.equalsIgnoreCase("POST")) {
+			requestBuilder.POST(HttpRequest.BodyPublishers.ofString(""));
+		} else if (apiMethod.equalsIgnoreCase("PUT")) {
+			requestBuilder.PUT(HttpRequest.BodyPublishers.ofString(""));
+		} else if (apiMethod.equalsIgnoreCase("DELETE")) {
+			requestBuilder.DELETE();
+		} else {
+			return null;
+		}
+		return requestBuilder.build();
+	}
+
+	@Test void getLastReading() {
+		assertRequestStatus(getRequestForPath("/readings/last", "GET"), 200);
+	}
+
+	@Test void getAllReadings() {
+		assertRequestStatus(getRequestForPath("/readings", "GET"), 200);
+	}
 }
