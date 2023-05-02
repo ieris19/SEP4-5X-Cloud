8353134d9611d89bc06763e4cdbef0212d96a49f
Ieris19 <ieris19@outlook.com>
2023-05-02 14:38:18 +0200

    Added Last Reading Static Field
    Placeholder for a future Database, this static field just stores the last value for ease of retrieval.

--- src/main/java/dk/via/sep4/cloud/Persistance/SensorReading.java
+++ src/main/java/dk/via/sep4/cloud/Persistance/SensorReading.java
@@ -3,9 +3,15 @@ package dk.via.sep4.cloud.Persistance;
 import java.io.File;
 
 public class SensorReading {
+    private static SensorReading lastReading;
     private double temperature;
     private File file;
     private FileLog logger;
+
+
+    public static SensorReading getLastReading() {
+        return SensorReading.lastReading;
+    }
 
     public SensorReading(Double temperature)
     {
@@ -13,6 +19,7 @@ public class SensorReading {
         this.file=new File("src/main/java/dk/via/sep4/cloud/Persistance/Logs.txt");
         this.logger=FileLog.getInstance(file);
         logReadings();
+        SensorReading.lastReading=this;
     }
     private void logReadings()
     {
