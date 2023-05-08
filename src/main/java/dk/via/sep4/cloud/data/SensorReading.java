ce9c578cbc937e58daff5179d1e2faac3123c804
Ieris19 <ieris19@outlook.com>
2023-05-08 18:42:25 +0200

    Create SensorRepository
    Created a SensorRepository to interact with a MongoDB database. For now it only connects to local databases, but it should be easy enough to wiggle eventually into a fully working db

---
+++ src/main/java/dk/via/sep4/cloud/data/SensorReading.java
@@ -0,0 +1,41 @@
+package dk.via.sep4.cloud.data;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.sql.Timestamp;
+
+@Data
+public class SensorReading {
+	@Id
+	private String id;
+	private boolean open;
+	private boolean pir;
+	private double temperature;
+	private int humidity;
+	private int co2;
+	private int sound;
+	private int light;
+	private int code;
+	private Timestamp timeReceived;
+
+	public SensorReading() {
+
+	}
+
+	public SensorReading(boolean open, boolean pir, double temperature, int humidity, int co2, int sound, int light, int code, Timestamp timeReceived) {
+		this.open = open;
+		this.pir = pir;
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.co2 = co2;
+		this.sound = sound;
+		this.light = light;
+		this.code = code;
+		this.timeReceived = timeReceived;
+	}
+
+	@Override public String toString() {
+		return String.format("Reading[open=%s, pir=%s, temperature=%s, humidity=%s, co2=%s, sound=%s, light=%s, code=%s, timeReceived=%s]", open, pir, temperature, humidity, co2, sound, light, code, timeReceived);
+	}
+}
