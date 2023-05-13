cbe29199915513460123e177576ff6d18276d0d5
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-13 11:48:30 +0200

    Multiple changes: 1. DBrepository now has update methods for all limit instances 2. DBrepository has a setUp method that can be changed and used after database reset 3. SensorLimits class will store sensor limits. It will be used throughout the system

--- src/main/java/dk/via/sep4/cloud/data/DBrepository.java
+++ src/main/java/dk/via/sep4/cloud/data/DBrepository.java
@@ -4,6 +4,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
 import dk.via.sep4.cloud.data.repository.SensorRepository;
 import org.bson.Document;
 
@@ -18,11 +19,12 @@ public class DBrepository {
         this.db=client.getDatabase("SEP4DB");
         this.collection=db.getCollection("SEP4COLLECTION");
         System.out.println("Connected to DB");
+        UpdateOptions options = new UpdateOptions().upsert(true);
     }
 
     public void insertReading(SensorReading reading)
     {
-        Document DBreading = new Document("isOpen", reading.isOpen())
+        Document DBreading = new Document("type", "reading")
                 .append("pir", reading.isPir())
                 .append("temperature", reading.getTemperature())
                 .append("humidity", reading.getHumidity())
@@ -35,4 +37,87 @@ public class DBrepository {
         collection.insertOne(DBreading);
     }
 
+    public void updateTemperatureLimit(SensorLimits limits)
+    {
+        Document filter=new Document("_id", 1);
+
+        Document DBlimit=new Document("_id", 1)
+                .append("type", "temperature limit")
+                .append("value", limits.getTemperature());
+
+        collection.findOneAndDelete(filter);
+        collection.insertOne(DBlimit);
+    }
+    public void updateHumidityLimit(SensorLimits limits)
+    {
+        Document filter=new Document("_id", 2);
+
+        Document DBlimit=new Document("_id", 2)
+                .append("type", "humidity limit")
+                .append("value", limits.getHumidity());
+
+        collection.findOneAndDelete(filter);
+        collection.insertOne(DBlimit);
+    }
+    public void updateCo2Limit(SensorLimits limits)
+    {
+        Document filter=new Document("_id", 3);
+
+        Document DBlimit=new Document("_id", 3)
+                .append("type", "co2 limit")
+                .append("value", limits.getCo2());
+
+        collection.findOneAndDelete(filter);
+        collection.insertOne(DBlimit);
+    }
+    public void updateSoundLimit(SensorLimits limits)
+    {
+        Document filter=new Document("_id", 4);
+
+        Document DBlimit=new Document("_id", 4)
+                .append("type", "sound limit")
+                .append("value", limits.getSound());
+
+        collection.findOneAndDelete(filter);
+        collection.insertOne(DBlimit);
+    }
+    public void updateLightLimit(SensorLimits limits)
+    {
+        Document filter=new Document("_id", 5);
+
+        Document DBlimit=new Document("_id", 5)
+                .append("type", "light limit")
+                .append("value", limits.getLight());
+
+        collection.findOneAndDelete(filter);
+        collection.insertOne(DBlimit);
+    }
+
+    public void setUp(SensorLimits limits) {
+        Document temperatureLimit=new Document("_id", 1)
+                .append("type", "temperature limit")
+                .append("value", limits.getTemperature());
+
+        Document humiditylimit=new Document("_id", 2)
+                .append("type", "humidity limit")
+                .append("value", limits.getHumidity());
+
+        Document co2Limit=new Document("_id", 3)
+                .append("type", "co2 limit")
+                .append("value", limits.getCo2());
+
+        Document soundLimit=new Document("_id", 4)
+                .append("type", "sound limit")
+                .append("value", limits.getSound());
+
+        Document lightLimit=new Document("_id", 5)
+                .append("type", "light limit")
+                .append("value", limits.getLight());
+
+        collection.insertOne(temperatureLimit);
+        collection.insertOne(humiditylimit);
+        collection.insertOne(co2Limit);
+        collection.insertOne(soundLimit);
+        collection.insertOne(lightLimit);
+    }
 }
