3da0350b624347473c7907645726fce152f5091c
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-13 11:48:30 +0200

    Multiple changes: 1. DBrepository now has update methods for all limit instances 2. DBrepository has a setUp method that can be changed and used after database reset 3. SensorLimits class will store sensor limits. It will be used throughout the system

--- src/main/java/dk/via/sep4/cloud/data/DBapplication.java
+++ src/main/java/dk/via/sep4/cloud/data/DBapplication.java
@@ -13,6 +13,24 @@ import java.time.LocalDateTime;
 public class DBapplication {
     public static void main(String[] args) {
         DBrepository database=new DBrepository();
-        database.insertReading(new SensorReading(true, true, 27, 30, 15, 1, 5, 404, Timestamp.valueOf(LocalDateTime.of(2023, 5, 12, 13, 4, 15))));
+
+        //Test insert
+        //database.insertReading(new SensorReading(true, 27, 30, 15, 1, 5, 404, Timestamp.valueOf(LocalDateTime.of(2023, 5, 12, 13, 4, 15))));
+
+        //Setting up limits
+        SensorLimits limits=new SensorLimits();
+
+        limits.setTemperature(27);
+        limits.setHumidity(27);
+        limits.setCo2(27);
+        limits.setSound(27);
+        limits.setLight(27);
+
+        database.updateTemperatureLimit(limits);
+        database.updateHumidityLimit(limits);
+        database.updateCo2Limit(limits);
+        database.updateSoundLimit(limits);
+        database.updateLightLimit(limits);
+//      database.setUp(limits);
     }
 }
