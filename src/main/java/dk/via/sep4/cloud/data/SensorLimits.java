a736b1667dc51cfd01a3e66f176360d7329ae425
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-14 12:31:30 +0200

    Multiple small changes: 1. SensorReading and SensorLimits classes had their getters and setters removed as we have an @Data anotation on both of those classes 2. DBapplication is literally a testing ground, dont mind it

--- src/main/java/dk/via/sep4/cloud/data/SensorLimits.java
+++ src/main/java/dk/via/sep4/cloud/data/SensorLimits.java
@@ -1,5 +1,8 @@
 package dk.via.sep4.cloud.data;
 
+import lombok.Data;
+
+@Data
 public class SensorLimits {
     private double temperature;
     private int humidity;
@@ -7,48 +10,7 @@ public class SensorLimits {
     private int sound;
     private int light;
 
-    public SensorLimits()
-    {
+    public SensorLimits(){
 
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
-    public int getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(int humidity) {
-        this.humidity = humidity;
-    }
-
-    public int getCo2() {
-        return co2;
-    }
-
-    public void setCo2(int co2) {
-        this.co2 = co2;
-    }
-
-    public int getSound() {
-        return sound;
-    }
-
-    public void setSound(int sound) {
-        this.sound = sound;
-    }
-
-    public int getLight() {
-        return light;
-    }
-
-    public void setLight(int light) {
-        this.light = light;
     }
 }
