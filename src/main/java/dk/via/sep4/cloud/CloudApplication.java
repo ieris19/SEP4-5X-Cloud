18c85a1c583af45ffd6a5c3e4c4d41d525b5f2d6
Ieris19 <ieris19@outlook.com>
2023-05-14 02:43:18 +0200

    Fixed configuration
    Added secrets.properties to the .gitignore, in order to remove secrets from github.
    
    Also fixed the CloudApplication class because nothing should really need to change it, as it is pure boilerplate

--- src/main/java/dk/via/sep4/cloud/CloudApplication.java
+++ src/main/java/dk/via/sep4/cloud/CloudApplication.java
@@ -1,15 +1,12 @@
 package dk.via.sep4.cloud;
 
 
-import dk.via.sep4.cloud.lorawan.C;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CloudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
-		SpringApplication.run(C.class,args);
 	}
 }
