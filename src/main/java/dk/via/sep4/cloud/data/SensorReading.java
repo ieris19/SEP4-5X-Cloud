16d276a44a08acb335154e0fe52d170fed20b565
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-14 17:00:52 +0200

    1. Created a SensorReading constructor that initializes a class object by taking a JSON string 2. getReadings() method in DBrepository now returns an array of SensorReading objects

--- src/main/java/dk/via/sep4/cloud/data/SensorReading.java
+++ src/main/java/dk/via/sep4/cloud/data/SensorReading.java
@@ -1,9 +1,14 @@
 package dk.via.sep4.cloud.data;
 
 import lombok.Data;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class SensorReading {
@@ -31,6 +36,26 @@ public class SensorReading {
 		this.code = code;
 		this.timeReceived = timeReceived;
 	}
+
+	public SensorReading(String jsonString) {
+		JSONObject dataJson = new JSONObject(jsonString.toString());
+		JSONObject idJson = dataJson.getJSONObject("_id");
+		this.id=idJson.getString("$oid");
+		this.pir=dataJson.getBoolean("pir");
+		this.temperature = dataJson.getDouble("temperature");
+		this.humidity = dataJson.getInt("humidity");
+		this.co2 = dataJson.getInt("co2");
+		this.sound = dataJson.getInt("sound");
+		this.light = dataJson.getInt("light");
+		this.code = dataJson.getInt("code");
+		JSONObject dateJson = dataJson.getJSONObject("time");
+		JSONObject timeJson = new JSONObject(dateJson.toString());
+
+		String pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern(pattern);
+		this.timeReceived=Timestamp.valueOf(LocalDateTime.parse(timeJson.getString("$date"), dtf));
+	}
+
 	@Override public String toString() {
 		return String.format("Reading[pir=%s, temperature=%s, humidity=%s, co2=%s, sound=%s, light=%s, code=%s, timeReceived=%s]", pir, temperature, humidity, co2, sound, light, code, timeReceived);
 	}
