9085ae71b92e879266d3471b423d4185746b64e1
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-14 12:33:26 +0200

    Deleted SensorRepository interface from the system as we are not using it

--- src/main/java/dk/via/sep4/cloud/data/repository/SensorRepository.java
+++
@@ -1,7 +0,0 @@
-package dk.via.sep4.cloud.data.repository;
-
-import dk.via.sep4.cloud.data.SensorReading;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SensorRepository extends MongoRepository<SensorReading, String> {
-}
