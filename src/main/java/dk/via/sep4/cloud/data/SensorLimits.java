6b65e94fd4d54aa7ef7225901ad09bd4f79c6151
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-15 16:18:36 +0200

    Fixed SensorLimits to not have min co2 values, no sound or light measurements

--- src/main/java/dk/via/sep4/cloud/data/SensorLimits.java
+++ src/main/java/dk/via/sep4/cloud/data/SensorLimits.java
@@ -9,28 +9,18 @@ public class SensorLimits {
     private double maxTemperature;
     private int minHumidity;
     private int maxHumidity;
-    private int minCo2;
     private int maxCo2;
-    private int minSound;
-    private int maxSound;
-    private int minLight;
-    private int maxLight;
 
     public SensorLimits(){
 
     }
 
-    public SensorLimits(double minTemperature, double maxTemperature, int minHumidity, int maxHumidity, int minCo2, int maxCo2, int minSound, int maxSound, int minLight, int maxLight) {
+    public SensorLimits(double minTemperature, double maxTemperature, int minHumidity, int maxHumidity, int maxCo2) {
         this.minTemperature = minTemperature;
         this.maxTemperature = maxTemperature;
         this.minHumidity = minHumidity;
         this.maxHumidity = maxHumidity;
-        this.minCo2 = minCo2;
         this.maxCo2 = maxCo2;
-        this.minSound = minSound;
-        this.maxSound = maxSound;
-        this.minLight = minLight;
-        this.maxLight = maxLight;
     }
 
     public SensorLimits(String jsonString) {
@@ -39,11 +29,6 @@ public class SensorLimits {
         this.maxTemperature= dataJson.getDouble("maxTemperature");
         this.minHumidity = dataJson.getInt("minHumidity");
         this.maxHumidity = dataJson.getInt("maxHumidity");
-        this.minCo2 = dataJson.getInt("minCo2");
         this.maxCo2 = dataJson.getInt("maxCo2");
-        this.minSound = dataJson.getInt("minSound");
-        this.maxSound = dataJson.getInt("maxSound");
-        this.minLight = dataJson.getInt("minLight");
-        this.maxLight = dataJson.getInt("maxLight");
     }
 }
