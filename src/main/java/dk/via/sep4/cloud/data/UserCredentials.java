922788343111aa959a84b0dd60b88b959ad5f61f
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-15 16:52:36 +0200

    Created UserCredentials class

---
+++ src/main/java/dk/via/sep4/cloud/data/UserCredentials.java
@@ -0,0 +1,25 @@
+package dk.via.sep4.cloud.data;
+
+import lombok.Data;
+import org.json.JSONObject;
+
+@Data
+public class UserCredentials {
+    private String username;
+    private String password;
+
+    public UserCredentials() {
+
+    }
+
+    public UserCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserCredentials(String jsonString) {
+        JSONObject dataJson = new JSONObject(jsonString);
+        this.username = dataJson.getString("username");
+        this.password = dataJson.getString("password");
+    }
+}
