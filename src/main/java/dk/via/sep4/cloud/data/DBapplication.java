acc3e2acb551782c0336e2fe191111ad870c67c7
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-15 16:29:13 +0200

    Fixed up sensor limit stuff

--- src/main/java/dk/via/sep4/cloud/data/DBapplication.java
+++ src/main/java/dk/via/sep4/cloud/data/DBapplication.java
@@ -7,13 +7,26 @@ import java.time.LocalDateTime;
 
 public class DBapplication {
     public static void main(String[] args) throws JsonProcessingException {
+        //DB repo initialization
         DBrepository database=new DBrepository();
 
-        database.insertReading(new SensorReading(true, 41, 80, 369, 42, 150, 527, Timestamp.valueOf(LocalDateTime.of(2023, 5, 15, 13, 22, 23))));
+        //Inserting reading example
+//        database.insertReading(new SensorReading(true, 41, 80, 369, 42, 150, 527, Timestamp.valueOf(LocalDateTime.of(2023, 5, 15, 13, 22, 23))));
+//
+//        //Retrieving readings example
+//        SensorReading[] list= database.getReadings();
+//        for (SensorReading reading:list) {
+//            System.out.println(reading.toString());
+//        }
+//
+//        //Inserting limits example
+//        database.setUp(new SensorLimits(10, 35, 20, 80, 1000));
+//
+//        //Retrieving limits example
+//        System.out.println(database.getLimits().toString());
 
-        SensorReading[] list= database.getReadings();
-        for (SensorReading reading:list) {
-            System.out.println(reading.toString());
-        }
+        //Updating limits example
+        database.updateLimits(new SensorLimits(10, 35, 20, 80, 3000));
+        System.out.println(database.getLimits());
     }
 }
