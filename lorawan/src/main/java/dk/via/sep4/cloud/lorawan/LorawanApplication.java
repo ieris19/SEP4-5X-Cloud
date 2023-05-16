1d0e4a4cd80882f58547437c0d2df2c488bcfec4
Ieris19 <ieris19@outlook.com>
2023-05-16 19:21:44 +0200

    Added Springboot Applications
    Springboot applications to launch the modules individually. However, data module is a bean and not an application so a configuration class instantiating the bean has been provided

---
+++ lorawan/src/main/java/dk/via/sep4/cloud/lorawan/LorawanApplication.java
@@ -0,0 +1,8 @@
+package dk.via.sep4.cloud.lorawan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication(scanBasePackages = {"dk.via.sep4.cloud.lorawan", "dk.via.sep4.cloud.data"})
+public class LorawanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LorawanApplication.class, args);
+    }
