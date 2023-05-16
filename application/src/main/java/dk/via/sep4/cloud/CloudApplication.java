9ec0aef4dc6b84ff43210cb7c0089d3c32f2317e
Ieris19 <ieris19@outlook.com>
2023-05-16 19:06:06 +0200

    Aggregated application module
    Fixed the aggregated application module. Basically, this module allows running the application in a single Spring container, as it was before. This is temporary

--- application/src/main/java/dk/via/sep4/cloud/CloudApplication.java
+++ application/src/main/java/dk/via/sep4/cloud/CloudApplication.java
@@ -4,9 +4,9 @@ package dk.via.sep4.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"dk.via.sep4.cloud.web", "dk.via.sep4.cloud.lorawan", "dk.via.sep4.cloud.data"})
 public class CloudApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
 }
