027f1f551f7ad3d7743d641dce33c3712aaec2d7
Ieris19 <ieris19@outlook.com>
2023-05-18 15:06:30 +0200

    Fixed use of secrets in container
    Container now uses a initializing script that will read the secrets from the environmental variables.

---
+++ web/init.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Start-up script for the web api service
+echo "Starting web api service..."
+
+# Create secret properties file from environment variables
+echo "Creating secret properties file..."
+mkdir config
+touch config/secrets.properties
+echo "mongodb.url=${mongodb_url}" >> config/secrets.properties
+echo "Secrets file created."
+
+echo "Starting Spring Container..."
+java -jar web.jar
+echo "Spring Container has terminated"
