304b9870d9157d1c37154c5c56d4f0c7ebb6bb10
Ieris19 <ieris19@outlook.com>
2023-05-18 15:48:39 +0200

    Changed port mapping for start-local.sh
    Now it maps the container's 8080 port to the host's 8081 port such that the app can locally run in and out of docker simultaneously

--- web/start-local.sh
+++ web/start-local.sh
@@ -4,4 +4,4 @@
 echo "Starting web api service container..."
 
 docker build -t web-api-service:latest .
-docker run -dp 8080:8080 --env-file ./../config/secrets.env --name web-api-service web-api-service:latest
+docker run -dp 8081:8080 --env-file ./../config/secrets.env --name web-api-service web-api-service:latest
