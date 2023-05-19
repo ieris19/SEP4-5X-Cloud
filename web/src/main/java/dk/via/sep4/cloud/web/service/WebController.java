7b8266119cd60b62d0001e1703484de2d2db8377
Ieris19 <ieris19@outlook.com>
2023-05-20 00:03:29 +0200

    Error Handling for the API
    The API will now actually reply in case of an internal server error whenever an exception is thrown. For now, it will just reply with information on the exception that occurred, but hoping that is better than nothing

--- web/src/main/java/dk/via/sep4/cloud/web/service/WebController.java
+++ web/src/main/java/dk/via/sep4/cloud/web/service/WebController.java
@@ -1,11 +1,11 @@
 package dk.via.sep4.cloud.web.service;
 
 import dk.via.sep4.cloud.web.data.WebRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.sql.Timestamp;
 
 /**
  * This class is used to create a REST API for the web application.
@@ -16,6 +16,7 @@ import java.sql.Timestamp;
 @RequestMapping()
 public class WebController {
     private final WebRepository repository;
+    private final Logger logger = LoggerFactory.getLogger(WebController.class);
 
     @Autowired
     public WebController(WebRepository webRepository) {
@@ -23,14 +24,31 @@ public class WebController {
     }
 
     @GetMapping("/readings")
-    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
     public ResponseEntity<String> getReadings(@RequestParam String date) {
-        return ResponseEntity.ok(repository.getReadings(date));
+        try {
+            return ResponseEntity.ok(repository.getReadings(date));
+        } catch (Exception e) {
+            return handleException(e);
+        }
     }
 
     @GetMapping("/limits")
-    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
     public ResponseEntity<String> getLimits() {
-        return ResponseEntity.ok(repository.getLimits());
+        try {
+            return ResponseEntity.ok(repository.getLimits());
+        } catch (Exception e) {
+            return handleException(e);
+        }
+    }
+
+    private ResponseEntity<String> handleException(Exception e) {
+        logger.error("An internal error has occurred serving a request!", e);
+        Class<? extends Exception> errorClass = e.getClass();
+        StringBuilder errorMessage = new StringBuilder("An internal error has occurred: ");
+        errorMessage.append(errorClass.getSimpleName());
+        if (e.getMessage() != null) {
+            errorMessage.append("\nThe following details were provided: ").append(e.getMessage());
+        }
+        return ResponseEntity.internalServerError().body(errorMessage.toString());
     }
 }
