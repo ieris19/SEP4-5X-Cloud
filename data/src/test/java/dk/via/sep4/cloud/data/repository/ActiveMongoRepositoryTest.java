29eb8fd1d94ccc4fd97958e9a36ecf2e973f9126
Ieris19 <ieris19@outlook.com>
2023-05-19 15:52:51 +0200

    Modified the test
    It now runs on a new database, drops all contents of it, then proceeds to run operations on it and logs the progress

--- data/src/test/java/dk/via/sep4/cloud/data/repository/ActiveMongoRepositoryTest.java
+++ data/src/test/java/dk/via/sep4/cloud/data/repository/ActiveMongoRepositoryTest.java
@@ -1,47 +1,58 @@
 package dk.via.sep4.cloud.data.repository;
 
+import com.ieris19.lib.files.config.FileProperties;
 import dk.via.sep4.cloud.data.dto.SensorLimits;
 import dk.via.sep4.cloud.data.dto.SensorReading;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.URI;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 class ActiveMongoRepositoryTest {
     private static MongoRepository mongoRepository;
+    private static URI mongoURI;
+    private static Logger logger = LoggerFactory.getLogger("DB-TEST");
 
-    public static void main(String[] args) {
-        mongoRepository = new MongoRepository();
-        mongoRepository.init("mongodb://localhost:27017", "SEP4");
-//        Logger logger = LoggerFactory.getLogger("DB-TEST");
-//        //Inserting reading example
-//        mongoRepository.insertReading(new SensorReading(true, 41, 80, 369, 42, 150, 527, Timestamp.valueOf(LocalDateTime.of(2023, 5, 15, 13, 22, 23))));
-//        //Retrieving readings example
-//        SensorReading[] list = mongoRepository.getReadings("2023-05-15");
-//        System.out.println(list.toString());
-//
-//        for (SensorReading reading : list) {
-//            logger.info(reading.toString());
-//        }
-//
-//        //Inserting sensor limits example
-//        mongoRepository.insertLimits(new SensorLimits(10, 35, 20, 80, 1000));
-//
-//        //Retrieving limits example
-//        logger.info(mongoRepository.getLimits().toString());
-//
-//        //Updating limits example
-//        mongoRepository.updateLimits(new SensorLimits(10, 35, 20, 80, 3000));
-//        logger.info(mongoRepository.getLimits().toString());
-        System.out.println(mongoRepository.getReadings("2023-05-15").toString());
 
+    private static String getURI() {
+        try (FileProperties secret = FileProperties.getInstance("secrets")) {
+            return secret.getProperty("mongodb.url");
+        } catch (IllegalArgumentException e) {
+            throw new IllegalStateException("At least one of the accessed properties in secrets.properties doesn't exist", e);
+        } catch (IOException e) {
+            throw new IllegalStateException("Error reading secrets.properties", e);
+        }
     }
 
+    public static void main(String[] args) {
+        setUp();
+        //Inserting reading example
+        mongoRepository.insertReading(new SensorReading(true, 41, 80, 369, 42, 150, 527, Timestamp.valueOf(LocalDateTime.of(2023, 5, 15, 13, 22, 23))));
+//        //Retrieving readings example
+        SensorReading[] list = mongoRepository.getReadings("2023-05-15");
+//
+        for (SensorReading reading : list) {
+            logger.info(reading.toString());
+        }
+//
+//        //Inserting sensor limits example
+        mongoRepository.insertLimits(new SensorLimits(10, 35, 20, 80, 1000));
+//
+//        //Retrieving limits example
+        logger.info(mongoRepository.getLimits().toString());
+//
+//        //Updating limits example
+        mongoRepository.updateLimits(new SensorLimits(10, 35, 20, 80, 3000));
+        logger.info(mongoRepository.getLimits().toString());
+        logger.info(mongoRepository.getReadings("2023-05-15").toString());
+    }
 
     private static void setUp() {
         mongoRepository = new MongoRepository();
-        mongoRepository.init("mongodb://localhost:27017", "SEP4");
+        mongoRepository.init(getURI(), "SEP4");
         mongoRepository.clearEntireDatabase();
     }
 }
