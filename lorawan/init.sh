a22044f164fdbdb6569aa7752311b8b72642d7bf
Ieris19 <ieris19@outlook.com>
2023-05-22 17:48:13 +0200

    Fixed initialization scripts
    Shouldn't always append, first write should overwrite file just in case

--- lorawan/init.sh
+++ lorawan/init.sh
@@ -5,7 +5,7 @@ echo "Starting LoraWAN service..."
 echo "Creating secret properties file..."
 mkdir config
 touch config/secrets.properties
-echo "lorawan.url=${lorawan_url}" >> config/secrets.properties
+echo "lorawan.url=${lorawan_url}" > config/secrets.properties
 echo "lorawan.EUI_dev=${lorawan_EUI_dev}" >> config/secrets.properties
 echo "mongodb.url=${mongodb_url}" >> config/secrets.properties
 echo "Secrets file created."
