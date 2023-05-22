262d58b1776d0d3a6b74f5cd4a81f29dd75cafa3
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-22 13:16:08 +0200

    Created docker init shellscript

---
+++ lorawan/init.sh
@@ -0,0 +1,14 @@
+#Start-up script for the LoraWAN service
+echo "Starting LoraWAN service..."
+
+# Create secret properties file from environment variables
+echo "Creating secret properties file..."
+mkdir config
+touch config/secrets.properties
+echo "lorawan.url=${lorawan_url}" >> config/secrets.properties
+echo "lorawan.EUI_dev=${lorawan_EUI_dev}" >> config/secrets.properties
+echo "Secrets file created."
+
+echo "Starting Spring Container..."
+java -jar lorawan.jar
+echo "Spring Container has terminated"
