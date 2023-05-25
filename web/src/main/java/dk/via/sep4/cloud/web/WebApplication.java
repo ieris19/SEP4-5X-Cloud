d6f18bd4f7b63eb033f7a3b4aa75c4ec48ad8c6f
Rojus Paukste <rojus.ratkevicius@gmail.com>
2023-05-25 18:06:40 +0200

    Fixing up CORS again? Hopefully

--- web/src/main/java/dk/via/sep4/cloud/web/WebApplication.java
+++ web/src/main/java/dk/via/sep4/cloud/web/WebApplication.java
@@ -2,10 +2,24 @@ package dk.via.sep4.cloud.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"dk.via.sep4.cloud.web", "dk.via.sep4.cloud.data"})
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
