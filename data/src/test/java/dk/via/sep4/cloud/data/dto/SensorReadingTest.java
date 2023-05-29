600907bc98a7a1357c6c9f168f26970221904675
Ieris19 <ieris19@outlook.com>
2023-05-29 19:18:38 +0200

    Legacy Data parsing Test
    Test to ensure that legacy data can still be retrieved from the database

--- data/src/test/java/dk/via/sep4/cloud/data/dto/SensorReadingTest.java
+++ data/src/test/java/dk/via/sep4/cloud/data/dto/SensorReadingTest.java
@@ -13,9 +13,10 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class SensorReadingTest {
     private static String dataJSON;
-    private static String webJSON;
     private static String[] webJSONPairs;
     private static SensorReading sampleReading;
+    private static String legacyJSON;
+    private static SensorReading legacyReading;
 
     @BeforeAll
     static void setUp() {
@@ -38,20 +39,6 @@ class SensorReadingTest {
                           "comment": ""
                         }
                         """.trim().replace(" ", "").replace("\n", "");
-        webJSON =
-                """
-                        {
-                          "id": "6474c0a4c737a0417aefe675",
-                          "code": "100101",
-                          "temperature": 4240.5,
-                          "co2": 42405,
-                          "humidity": 165,
-                          "light": 2650,
-                          "sound": 42405,
-                          "pir": true,
-                          "time": "2023-05-29 17:11:32.517"
-                        }
-                        """.trim().replace(" ", "").replace("\n", "");
         webJSONPairs = new String[]{
                 "\"id\":\"6474c0a4c737a0417aefe675\"",
                 "\"code\":\"100101\"",
@@ -66,12 +53,39 @@ class SensorReadingTest {
         sampleReading = new SensorReading("6474c0a4c737a0417aefe675",
                 true, 4240.5, 165, 42405, 42405, 2650, "100101",
                 Timestamp.valueOf("2023-05-29 17:11:32.517"), "");
+        legacyJSON =
+                """
+                        {
+                          "_id": {
+                            "$oid": "6474c0a4c737a0417aefe675"
+                          },
+                          "pir": true,
+                          "temperature": 4240.5,
+                          "humidity": 165,
+                          "co2": 42405,
+                          "sound": 42405,
+                          "light": 2650,
+                          "code": 3,
+                          "time": {
+                            "$date": "2023-05-29T17:11:32.517Z"
+                           },
+                        }
+                        """.trim().replace(" ", "").replace("\n", "");
+        legacyReading = new SensorReading("6474c0a4c737a0417aefe675",
+                true, 4240.5, 165, 42405, 42405, 2650, "3",
+                Timestamp.valueOf("2023-05-29 17:11:32.517"), "");
     }
 
     @Test
     void fromJson() {
         SensorReading reading = SensorReading.fromJson(dataJSON);
         assertEquals(sampleReading, reading);
+    }
+
+    @Test
+    void fromLegacyJson() {
+        SensorReading test = SensorReading.fromJson(legacyJSON);
+        assertEquals(legacyReading, test);
     }
 
     @Test
